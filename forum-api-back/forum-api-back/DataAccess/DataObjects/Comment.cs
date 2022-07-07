adf1658e21a344e9f8ea98b3ef67289b75bef529
Lionel Naye <ngonkouralionel@gmail.com>
2022-07-07 10:00:27 +0200

    comment service repository et controller

--- forum-api-back/forum-api-back/DataAccess/DataObjects/Comment.cs
+++ forum-api-back/forum-api-back/DataAccess/DataObjects/Comment.cs
@@ -8,8 +8,8 @@ namespace forum_api_back.DataAccess.DataObjects
         public int Idcomment { get; set; }
         public string NomUtilisateur { get; set; } = null!;
         public string Contenu { get; set; } = null!;
-        public DateOnly DateCreation { get; set; }
-        public DateOnly? DateModification { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime? DateModification { get; set; }
         public int? TopicId { get; set; }
 
         public virtual Topic? Topic { get; set; }
--- forum-api-back/forum-api-back/Services/CommentService.cs
+++ forum-api-back/forum-api-back/Services/CommentService.cs
@@ -1,6 +1,71 @@
-﻿namespace forum_api_back.Services
+﻿using forum_api_back.DataAccess.DataObjects;
+using forum_api_back.Interfaces;
+using forum_api_back.Repositories;
+
+namespace forum_api_back.Services
 {
-    public class CommentService
+    public class CommentService : ICommentService
     {
+        private ICommentRepository repository;
+
+        public CommentService(ICommentRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Comment FindById(int id) 
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentException("Pas d'objet trouvé avec cet id : " + id);
+            }
+            return this.repository.FindById(id);
+        }
+
+        public List<Comment> FindAll()
+        {
+            List<Comment> comments = this.repository.FindAll();
+            if(comments == null)
+            {
+                throw new ArgumentNullException("Pas d'objet dans la base de données");
+            }
+            return comments;
+        }
+
+        public Comment Create(Comment comment)
+        {
+            if(comment == null)
+            {
+                throw new ArgumentNullException("L'objet est null");
+            }
+
+            if(comment.TopicId == 0)
+            {
+                throw new ArgumentNullException("Le commentaire n'est pas lié a un topic");
+            }
+
+            comment.DateCreation = DateTime.Now;
+            return this.repository.Create(comment);
+        }
+
+        public Comment Update(Comment comment)
+        {
+            if(comment == null)
+            {
+                throw new ArgumentNullException("L'objet est null");
+            }
+
+            comment.DateModification = DateTime.Now;
+            return this.repository.Update(comment);
+        }
+
+        public void Delete(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentNullException("id n'existe pas");
+            }
+            this.repository.Delete(id);
+        }
     }
 }
