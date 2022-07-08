bed799b95234f875513aa4e5f9393f1690d7e5f9
timothée <timothee.lafay.pro@gmail.com>
2022-07-08 13:39:40 +0200

    service filtre, test filtre

---
+++ forum-api-back/forum-api-back/Services/WorldFilterService.cs
@@ -0,0 +1,45 @@
+﻿using forum_api_back.Interfaces;
+
+namespace forum_api_back.Services
+{
+    public class WorldFilterService : IWorldFilterService
+    {
+        public string[] banWords;
+
+        public WorldFilterService()
+        {         
+            var contenuInsults = File.ReadLines(@"../../../../insults.txt");
+            banWords = contenuInsults.ToArray();
+        }
+
+        public string ChangeInsultToStar(string text)
+        {
+            // Tester si "text" n'est pas null et pas vide ?
+            if(text != null && text != "")
+            {
+                // Parcourir les mots du tableau banWord 
+                foreach(var mot in banWords)
+                {
+                    // Si dans le text il contient un mot du tableau insulte
+                    if (text.Contains(mot))
+                    {
+                        // Nouveau mot avec la première lettre du mot banni
+                        var nouveauMot = "" + mot[0];
+                        // À partir de la deuxième lettre jusqu'à l'avant dernière je remplace par une étoile
+                        for(int i = 1; i < mot.Length - 1; i++)
+                        {
+                            nouveauMot += "*";
+                        }
+                        // Ajoute la dernière lettre du mot banni
+                        nouveauMot += mot[mot.Length - 1];
+                        // Je remplace le mot du text par le nouveau avec les étoiles
+                        text = text.Replace(mot, nouveauMot);
+                    }
+                }
+            }
+            return text;
+        }
+
+
+    }
+}
